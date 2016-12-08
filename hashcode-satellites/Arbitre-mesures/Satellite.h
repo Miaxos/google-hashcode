9326aacc12b2610579ea1b44a5d94ce8d5455c73
Vincent Cotineau <vincentcotineau@gmail.com>
2016-12-08 09:27:31 +0100

    8/12_commit to pull

---
+++ hashcode-satellites/Arbitre-mesures/Satellite.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <ostream>
+#include <vector>
+using namespace std;
+
+class Satellite
+{
+public:
+
+	//Position du satellite au tour i
+	vector<int> pos_tour(int i);
+
+	// Getters
+	inline int getOrientLat() const
+	{
+		return m_orientLat;
+	}
+
+	inline int getOrientLong() const
+	{
+		return m_orientLong;
+	}
+
+	inline unsigned int getId() const
+	{
+		return m_id;
+	}
+
+	static const int VIT_LONG = -15; /*Vitesse du satellite en longitude, correspondant à la vitesse 
+	de rotation de la Terre*/
+
+private:
+
+	static unsigned int m_idCount;
+
+	unsigned int m_id; // l'id du satellite
+
+	int m_latitude; // position du satellite en latitude
+	int m_longitude; // position du satellite en longitude
+	int m_vitLat; // vitesse du satellite en latitude
+
+	int m_orientLat; // orientation en latitude
+	int m_orientLong; // orientation en longitude
+	int m_orientMaxTour; // changement d'orientation max par tour
+	int m_orientMaxTotal; // orientation max totale
+};
--- hashcode-satellites/Resolution/Satellite.hpp
+++ hashcode-satellites/Resolution/Satellite.hpp
@@ -30,13 +30,8 @@ class Satellite
         */
         void tourSuivant(int orientLat, int orientLong);
 
-        // Indique que le satellite prend la photo donnée au tour donné
-        void prendrePhoto(Photo& photo, unsigned int tour);
-
-        // Recherche la prochaine photo qui peut être prise et indique dans combien de tours elle peut être prise
-        // Pré-condition : les photos sont triées
-        EtatSatellitePhoto prochainePhoto(const std::vector<Photo*>& photos, unsigned int tourMax);
-
+        
+       
 
         // Getters
         inline int getOrientLat() const
